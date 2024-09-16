3e1e62e7f66230ab0a821ba013a533db340f8381
DuongHiepp <hiepdh@icebear.com.vn>
2024-09-16 16:41:44 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,61 @@
+
+
+const BestPA = [
+    {   
+        timeline: "Tháng 8:", 
+        data: [
+            {
+                path: "/BestPA/Month8_2024/Preview_BilliardPuzzle_Playable_OP13_Drag_GP_LongFlowSpecTable.html",
+                name: "Preview_BilliardPuzzle_Playable_OP13_Drag_GP_LongFlowSpecTable",
+                dev: "Phong",
+            },
+            {
+                path: "/BestPA/Month8_2024/Preview_MakeoverASMR_Playable_Op34_Drag_FGP_ButtMaker.html",
+                name: "Preview_MakeoverASMR_Playable_Op34_Drag_FGP_ButtMaker",
+                dev: "Phong",
+            },
+            {
+                path: "/BestPA/Month8_2024/preview_MakeoverASMR_Playable_Op38_Click_GP_Heart.html",
+                name: "preview_MakeoverASMR_Playable_Op38_Click_GP_Heart",
+                dev: "Quân",
+            },
+            {
+                path: "/BestPA/Month8_2024/Preview_MergeCraftRobo_Playable_Applovin_Op1_GP_Click_Merge.html",
+                name: "Preview_MergeCraftRobo_Playable_Applovin_Op1_GP_Click_Merge",
+                dev: "Quân",
+            },
+            {
+                path: "/BestPA/Month8_2024/Preview_MergeCraftRobo_Playable_Op9_Click_GP_MergePixel.html",
+                name: "Preview_MergeCraftRobo_Playable_Op9_Click_GP_MergePixel",
+                dev: "Quân",
+            },
+            {
+                path: "/BestPA/Month8_2024/Preview_Statisdom_Playable_Op7_Click_GP_Lv7_Mid_Autumn.html",
+                name: "Preview_Statisdom_Playable_Op7_Click_GP_Lv7_Mid_Autumn",
+                dev: "Quân",
+            },
+            {
+                path: "/BestPA/Month8_2024/Preview_SuperToy_Playable_Op1_Click_GP_OpenEgg.html",
+                name: "Preview_SuperToy_Playable_Op1_Click_GP_OpenEgg",
+                dev: "Quân",
+            },
+            {
+                path: "/BestPA/Month8_2024/Preview_SuperToy_Playable_Op2_Click_GP_GachaEgg.html",
+                name: "Preview_SuperToy_Playable_Op2_Click_GP_GachaEgg",
+                dev: "Quân",
+            },
+            {
+                path: "/BestPA/Month8_2024/Preview_SuperToy3D_Playable_Op5_Halloween_1TP.html",
+                name: "Preview_SuperToy3D_Playable_Op5_Halloween_1TP",
+                dev: "Quân",
+            },
+            {
+                path: "/BestPA/Month8_2024/SuperToy_Playable_Applovin_Op4_Click_GP_From_Sensor.html",
+                name: "SuperToy_Playable_Applovin_Op4_Click_GP_From_Sensor",
+                dev: "Quân",
+            },
+        ]
+    }
+]
+
+export default BestPA;

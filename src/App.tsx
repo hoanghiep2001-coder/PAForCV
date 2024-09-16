9578c0a55ec69f1e9abf2a319ccacc9de5cf7136
DuongHiepp <hiepdh@icebear.com.vn>
2024-09-16 17:35:22 +0700

    update Pa

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,45 @@ function App() {
     <div className="App">
 
       <div className="overlay">
-      <div className="inner">
-        <h3 className="title">Best IceBear PA 2024!</h3>
-        <ul className="list">
-          <li className="item">
-            <p className="content">{BestPA[0].timeline}</p>
-
-            <ul className="sub-list">
-              {BestPA[0].data.map((PA_info, index) => {
-                return (
-                  <li key={index} className="sub-item">
-
-                    <p className="sub-item-dev">
-
-                      - {" " + PA_info.dev}
-
-                    </p>
-
-                    <a href={PA_info.path} className="link" target="_blank">
-                      - {" " + PA_info.name}
-                    </a>
+        <div className="inner">
+          <h3 className="title">Best IceBear PA 2024!</h3>
+          <ul className="list">
 
 
-                  </li>
-                )
-              })}
+            {BestPA.map((month, index) => {
+              return (
 
+                <li className="item">
+                  <div key={index}>
+                    <p className="content">{month.timeline}</p>
 
-            </ul>
-          </li>
-        </ul>
+                    <ul className="sub-list">
+                      {BestPA[0].data.map((PA_info, index) => {
+                        return (
+                          <li key={index} className="sub-item">
+
+                            <p className="sub-item-dev">
+                              - {" " + PA_info.dev}:
+                            </p>
+
+                            <a href={PA_info.path} className="link" target="_">
+                                {" " + PA_info.name}
+                            </a>
+                          </li>
+                        )
+                      })}
+
+                    </ul>
+                  </div>
+                </li>
+
+              )
+            })}
+          </ul>
+        </div>
       </div>
-      </div>
 
- 
+
     </div>
   );
 }
